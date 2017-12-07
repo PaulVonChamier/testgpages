a917cd68d2b99e4cb1bae01b9209a8bb06ca1cff
PaulVonChamier <31542950+PaulVonChamier@users.noreply.github.com>
2017-12-07 14:58:48 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//Define the queue
+
+$('#link1').click(function(){
+    $("html, body").animate({ scrollTop: 750 }, 600);
+    return false;
+});
+$('#link2').click(function(){
+    $("html, body").animate({ scrollTop: 1515 }, 600);
+    return false;
+});
+$('#link3').click(function(){
+    $("html, body").animate({ scrollTop: 2300 }, 600);
+    return false;
+});
+$('#link4').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+$('#link5').click(function(){
+    $("html, body").animate({ scrollTop: 1515 }, 600);
+    return false;
+});
+$('#link6').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+$('#link7').click(function(){
+    $("html, body").animate({ scrollTop: 2300 }, 600);
+    return false;
+});
+$('#link8').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
